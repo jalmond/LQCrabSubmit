0123ac92a8b5344b507bc504da7a71cf365233d6
jalmond <jalmond@cern.ch>
2014-03-17 11:25:34 +0000

    Add initial submit code

---
+++ Data/runscript.py
@@ -0,0 +1,272 @@
+import os,sys
+from functions import *
+
+type="Data"
+##### First entry is directory name
+
+EMuons = ["ElectronMuon" ,"MuEGA"]
+Muons = ["SingleMuon" ,"MuA", "MuB", "MuC", "MuD"]
+DoubleMuon = ["DoubleMuon" ,"DiMuB","DiMuC","DiMuD"]
+DoubleElectron = ["DoubleElectron" ,"DiElA"]
+
+list = DoubleMuon
+#list = DoubleElectron
+#list = EMuons
+
+resubmit_stuck =False
+
+##############################################################
+#### Add choice of site
+###
+###############################################################
+
+#### IF YOU WISH TO PRODUCE A SECOND SAMPLE
+### put any sample name in Extension and it will be resubmitted with new output name  
+Extension= []
+ext="V1"
+
+
+###### INITIALISE
+dir=""
+nsamples=0
+not_complete = []
+n_running = 0
+for i in list:
+    nsamples+=1
+    if nsamples ==1:
+        dir= i
+        continue
+
+    if not (os.path.exists(dir)):
+        os.system("mkdir " +  dir)
+
+    dataset=""
+    filename = 'datasetlist.txt'
+    for line in open(filename, 'r'):
+        if not line.startswith("#"):
+            entries = line.split()
+            if len(entries)==2:
+                if i == entries[0]:
+                    dataset=entries[1]
+
+    
+    for j in Extension:
+        if i == j : i = i+ ext
+        
+
+    cfgfile=  dir+"/ntupleCrab_DATA_" + i + ".cfg"
+    jobfile = dir + "/" + i + "/"
+    crablog = dir + "/" + i  + "/log/crab.log"
+    search="Creator: total # of jobs = "
+    search_sub = "Submitted job # " 
+    njobs=0
+
+    #### DOES THE DIR EXIST?
+    if not (os.path.exists(cfgfile)):
+        configfile=open(cfgfile,'w')
+        configfile.write(makeConfigFile(dataset, i, dir))
+        configfile.close()
+        os.system("crab -create -cfg " + cfgfile )
+    ##### DOES THE JOB EXIST?
+    elif not (os.path.exists(crablog)):    
+        os.system("crab -create -cfg " + cfgfile )
+    #### HAS THE JOB BEEN SUBMITTED    
+    else:
+        print "Job already created"
+
+
+
+    ######## CHECK IF ALL JOBS ARE SUBMITTED 
+    all_submitted=False
+       
+    logfile = open(crablog,'r')
+    found = False
+    for line in logfile:
+        if search in line:
+            strippedline = line[60:]
+            for s in strippedline.split():
+                njobs = s
+                print "Job has " + str(njobs) + " sub jobs"
+                break
+
+    logfile.close()
+    
+    search_sub+=str(njobs)
+    logfile = open(crablog,'r')
+    for line in logfile:
+        if search_sub in line:
+            all_submitted=True
+                
+    logfile.close()
+
+    if all_submitted == False:
+        ijobs= int(njobs)        
+        while ijobs > 0:
+            os.system("crab -submit 500 -c " + dir + "/" +  i )    
+            ijobs -= 500
+                
+    else:
+        print "Crab job exists for this file: Add extension and resubmit?"
+        os.system("crab -status -c " + dir+ "/" +  i)
+        os.system("crab -get -c " + dir+ "/" +  i)
+        os.system("crab -status -c " + dir+ "/" +  i)
+        os.system("crab -status -c " +dir+ "/"  + i + " > log.txt") 
+
+
+        
+        ##### check jobs that fail in output
+        
+        status_search = "Y   Retrieved         Cleared"
+        logfile = open("log.txt",'r')
+
+        ##### LIST FOR RESUBMITTING FAILED JOBS
+        resubmit_list = []
+        njob=0
+        for line in logfile:
+            if status_search in line:
+                strippedline = line[42:]
+                nstrips=0
+                for s in line.split():
+                    nstrips+=1
+                    if nstrips == 1:
+                        njob= int(s)
+                nsstrips=0        
+                for s in strippedline.split():
+                    nsstrips+=1
+                    if nsstrips < 3:
+                        if not int(s) == 0:
+                            print "Job " + str(njob) + " failed"           
+                            resubmit_list.append(njob)
+                            break
+                        
+        logfile.close()                    
+
+        ###### CHECK FOR ABORTED JOBS
+        status_search = "Y   Aborted           Aborted"
+        logfile = open("log.txt",'r')
+        njob=0
+        for line in logfile:
+            if status_search in line:
+                nstrips=0
+                for s in line.split():
+                    nstrips+=1
+                    if nstrips == 1:
+                        njob= int(s)
+                        print "Job " + s + " Aborted"
+                        resubmit_list.append(njob)
+                        break
+        logfile.close()
+
+        status_search = "N   Cancelled"
+        logfile = open("log.txt",'r')
+        njob=0
+        for line in logfile:
+            if status_search in line:
+                nstrips=0
+                for s in line.split():
+                    nstrips+=1
+                    if nstrips == 1:
+                        njob= int(s)
+                        resubmit_list.append(njob)
+                        break
+        logfile.close()
+                                                                                                                                                            
+                                                                                                                                                         
+
+        ##### RESUBMIT JOBS (FAILED+ ABORTED)
+        
+        fulllist = []
+        relist=""
+        nresubmit=0
+        for it in resubmit_list:
+            nresubmit+=1
+            if not nresubmit== 1:
+                relist+= ","
+            relist+= str(it)
+            if not it%400:
+                fulllist.append(relist)
+                relist=""
+        if not nresubmit == 0:
+            fulllist.append(relist)
+        
+        for k in fulllist:
+            resubmit_command = "crab -resubmit " + k + " -c " + dir+ "/" +  i
+            print resubmit_command
+            not_complete.append(i)
+            os.system(resubmit_command)
+
+
+
+        ########## COUNT NUMBER OF RUNNING JOBS
+        status_search = "N   Running"
+        n_running=0
+        logfile = open("log.txt",'r')
+        for line in logfile:
+            if status_search in line:
+                n_running+=1
+        logfile.close()
+
+
+        ######### ARE JOBS STILL SUBMITTED AND NOT RUNNING?
+        status_search = "N   Submitted         SubSuccess"
+        logfile = open("log.txt",'r')
+        stuck_list = []
+        njob=0
+        for line in logfile:
+            if status_search in line:
+                nstrips=0
+                for s in line.split():
+                    nstrips+=1
+                    if nstrips == 1:
+                        njob= int(s)
+                        stuck_list.append(njob)
+                        break
+        logfile.close()
+                    
+
+        
+                
+        ############  RESUBMIT STUCK JOBS?
+        stuck_fulllist = []
+        relist=""
+        nresubmit=0
+        for it in stuck_list:
+            nresubmit+=1
+            if not nresubmit== 1:
+                relist+= ","
+            relist+= str(it)
+            if not it%400:
+                stuck_fulllist.append(relist)
+                relist=""
+
+        if not nresubmit == 0:
+            stuck_fulllist.append(relist)
+
+        if resubmit_stuck:    
+            for k in stuck_fulllist:
+                kill_command = "crab -kill " + k + " -c " + dir+ "/" +  i
+                resubmit_command = "crab -resubmit " + k + " -c " + dir+ "/" +  i
+                print kill_command
+                print resubmit_command
+                not_complete.append(i)
+                os.system(kill_command)
+                os.system(resubmit_command)
+            
+        os.system("rm log.txt")   
+
+n_failed=0        
+for jobs in not_complete:
+    n_failed+=1
+
+if n_failed == 0:
+    if n_running ==0:
+        print "All jobs completed"
+    else:
+        print "Jobs still running. None failed"    
+else:
+    print "List of jobs yet that were resubmitted are:"
+    
+for jobs in not_complete:
+    print jobs
+    
+
---
+++ MC/runscript.py
@@ -0,0 +1,267 @@
+import os,sys
+from functions import *
+
+resubmit_stuck =False
+
+type="MC"
+##### First entry is directory name
+signallist_mumu = ["Signal_MuMu", "HNMuMu50", "HNMuMu70", "HNMuMu90", "HNMuMu100", "HNMuMu125", "HNMuMu150", "HNMuMu175", "HNMuMu200","HNMuMu225","HNMuMu250","HNMuMu275","HNMuMu300","HNMuMu325","HNMuMu350","HNMuMu375","HNMuMu400","HNMuMu500","HNMuMu600", "HNMuMu700"]
+
+signallist_ee = ["Signal_EE", "HNEE50", "HNEE70", "HNEE90", "HNEE100", "HNEE125", "HNEE150", "HNEE175", "HNEE200","HNEE225","HNEE250","HNEE275","HNEE300","HNEE325","HNEE350","HNEE375","HNEE400","HNEE500","HNEE600", "HNEE700"]
+
+DY = ["DY", "DY10to50", "DY50plus"]
+
+QCD = ["QCD", "QCD_mumu", "QCD_30to40EE", "QCD_40EE"]
+
+QCD2 = ["QCD", "QCD_mu15", "QCD_15to20mu", "QCD_20to30mu", "QCD_30to50mu", "QCD_50to80mu", "QCD_80to120mu", "QCD_120to170mu", "QCD_170to300mu", "QCD_300to470mu", "QCD_470to600mu", "QCD_600to800mu", "QCD_800to1000mu", "QCD_1000mu", "QCD_20to30E", "QCD_30to80E", "QCD_80to170E", "QCD_170to250E", "QCD_250to350E", "QCD_350E"]
+
+dibosonlist = ["Diboson", "WW","WZ","ZZ"]
+
+ttbar = ["Top", "ttbar"]
+
+W = ["Wjet", "WJets"]
+
+list = signallist_ee
+
+##############################################################
+#### Add choice of site
+###
+###############################################################
+
+Extension= "False"
+ext="V1"
+
+###### INITIALISE
+dir=""
+nsamples=0
+not_complete = []
+n_running = 0
+for i in list:
+    nsamples+=1
+    if nsamples ==1:
+        dir= i
+        continue
+
+    if not (os.path.exists(dir)):
+        os.system("mkdir " +  dir)
+
+    dataset=""
+    filename = 'datasetlist.txt'
+    for line in open(filename, 'r'):
+        if not line.startswith("#"):
+            entries = line.split()
+            if len(entries)==2:
+                if i == entries[0]:
+                    dataset=entries[1]
+
+    
+    if Extension == "True":
+        i = i+ ext
+        
+        
+    cfgfile=  dir+"/ntupleCrab_MC_" + i + ".cfg"
+    jobfile = dir + "/" + i + "/"
+    crablog = dir + "/" + i  + "/log/crab.log"
+    search="Creator: total # of jobs = "
+    search_sub = "Submitted job # " 
+    njobs=0
+
+    #### DOES THE DIR EXIST?
+    if not (os.path.exists(cfgfile)):
+        configfile=open(cfgfile,'w')
+        configfile.write(makeConfigFile(dataset, type, i, dir, i))
+        configfile.close()
+        os.system("crab -create -cfg " + cfgfile )
+    ##### DOES THE JOB EXIST?
+    elif not (os.path.exists(crablog)):    
+        os.system("crab -create -cfg " + cfgfile )
+    #### HAS THE JOB BEEN SUBMITTED    
+    else:
+        print "Job already created"
+
+    ######## CHECK IF ALL JOBS ARE SUBMITTED          
+    all_submitted=False
+        
+    logfile = open(crablog,'r')
+    found = False
+    for line in logfile:
+        if search in line:
+            strippedline = line[60:]
+            for s in strippedline.split():
+                njobs = s
+                print "Job has " + str(njobs) + " sub jobs"
+                break
+
+    logfile.close()
+    
+    search_sub+=str(njobs)
+    logfile = open(crablog,'r')
+    for line in logfile:
+        if search_sub in line:
+            all_submitted=True
+                
+    logfile.close()
+
+    if all_submitted == False:
+        ijobs= int(njobs)        
+        while ijobs > 0:
+            os.system("crab -submit 500 -c " + dir + "/" +  i )    
+            ijobs -= 500
+                
+    else:
+        print "Crab job exists for this file: Add extension and resubmit?"
+        os.system("crab -status -c " + dir+ "/" +  i)
+        os.system("crab -get -c " + dir+ "/" +  i)
+        os.system("crab -status -c " + dir+ "/" +  i)
+        os.system("crab -status -c " +dir+ "/"  + i + " > log.txt") 
+        
+        status_search = "Y   Retrieved         Cleared"
+        logfile = open("log.txt",'r')
+
+        ##### LIST FOR RESUBMITTING FAILED JOBS   
+        resubmit_list = []
+        njob=0
+        for line in logfile:
+            if status_search in line:
+                strippedline = line[42:]
+                nstrips=0
+                for s in line.split():
+                    nstrips+=1
+                    if nstrips == 1:
+                        njob= int(s)
+                nstrips=0        
+                for s in strippedline.split():
+                    nstrips+=1
+                    if nstrips < 3:
+                        if not int(s) == 0:
+                            resubmit_list.append(njob)
+                            break
+        logfile.close()                    
+        
+
+        ###### CHECK FOR ABORTED JOBS
+        status_search = "Y   Aborted           Aborted"
+        logfile = open("log.txt",'r')
+        njob=0
+        for line in logfile:
+            if status_search in line:
+                nstrips=0
+                for s in line.split():
+                    nstrips+=1
+                    if nstrips == 1:
+                        njob= int(s)
+                        print "Job " + s + " Aborted"
+                        resubmit_list.append(njob)
+                        break
+        logfile.close()
+        
+
+        status_search = "N   Cancelled"
+        logfile = open("log.txt",'r')
+        njob=0
+        for line in logfile:
+            if status_search in line:
+                nstrips=0
+                for s in line.split():
+                    nstrips+=1
+                    if nstrips == 1:
+                        njob= int(s)
+                        resubmit_list.append(njob)
+                        break
+        logfile.close()
+
+
+        ##### RESUBMIT JOBS (FAILED+ ABORTED)  
+        fulllist = []
+        relist=""
+        nresubmit=0
+        for it in resubmit_list:
+            nresubmit+=1
+            if not nresubmit== 1:
+                relist+= ","
+            relist+= str(it)
+            if not it%400:
+                fulllist.append(relist)
+                relist=""
+        if not nresubmit == 0:
+            fulllist.append(relist)
+        
+        for k in fulllist:
+            resubmit_command = "crab -resubmit " + k + " -c " + dir+ "/" +  i
+            print resubmit_command
+            not_complete.append(i)
+            os.system(resubmit_command)
+
+
+        ########## COUNT NUMBER OF RUNNING JOBS             
+        status_search = "N   Running"
+        n_running=0
+        logfile = open("log.txt",'r')
+        for line in logfile:
+            if status_search in line:
+                n_running+=1
+        logfile.close()
+
+        ######### ARE JOBS STILL SUBMITTED AND NOT RUNNING? 
+        status_search = "N   Submitted         SubSuccess"
+        logfile = open("log.txt",'r')
+        stuck_list = []
+        njob=0
+        for line in logfile:
+            if status_search in line:
+                nstrips=0
+                for s in line.split():
+                    nstrips+=1
+                    if nstrips == 1:
+                        njob= int(s)
+                        stuck_list.append(njob)
+                        break
+        logfile.close()
+                    
+
+
+
+        ############  RESUBMIT STUCK JOBS?
+        stuck_fulllist = []
+        relist=""
+        nresubmit=0
+        for it in stuck_list:
+            nresubmit+=1
+            if not nresubmit== 1:
+                relist+= ","
+            relist+= str(it)
+            if not it%400:
+                stuck_fulllist.append(relist)
+                relist=""
+
+        if not nresubmit == 0:
+            stuck_fulllist.append(relist)
+
+        if resubmit_stuck:    
+            for k in stuck_fulllist:
+                kill_command = "crab -kill " + k + " -c " + dir+ "/" +  i
+                resubmit_command = "crab -resubmit " + k + " -c " + dir+ "/" +  i
+                print kill_command
+                print resubmit_command
+                not_complete.append(i)
+                os.system(kill_command)
+                os.system(resubmit_command)
+                
+        os.system("rm log.txt")   
+
+n_failed=0        
+for jobs in not_complete:
+    n_failed+=1
+
+if n_failed == 0:
+    if n_running ==0:
+        print "All jobs completed"
+    else:
+        print "Jobs still running. None failed"    
+else:
+    print "List of jobs yet that were resubmitted are:"
+    
+for jobs in not_complete:
+    print jobs
+    
+
