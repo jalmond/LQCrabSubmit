56741301c79d90435e45e9e68ef527e53e160bd3
jalmond <jalmond@cern.ch>
2014-04-28 20:55:15 +0200

    update

--- Data/makelist.py
+++ Data/makelist.py
@@ -12,13 +12,13 @@ for stream in Stream:
     for period in periods:
         os.system("grep 'file =  rootTupleMaker_CRAB_DATA_2012_53X'   " + stream + "/" + dir[i_s]+ period +"/res/CMSSW_* > log" + stream +period)
     
-    frp = open('log'+ stream +period ,'r')
+        frp = open('log'+ stream +period ,'r')
     
-    fout = open(stream +'period'+ period +'.txt' ,'w')
-    for line in frp:
-        if "rootTupleMaker_CRAB" in line:
-            splitline  = line.split()
-            for sline in splitline:
-                if "rootTupleMaker_CRAB" in sline:
-                    fout.write(sline + "\n")
+        fout = open(stream +'period'+ period +'.txt' ,'w')
+        for line in frp:
+            if "rootTupleMaker_CRAB" in line:
+                splitline  = line.split()
+                for sline in splitline:
+                    if "rootTupleMaker_CRAB" in sline:
+                        fout.write(sline + "\n")
                 
